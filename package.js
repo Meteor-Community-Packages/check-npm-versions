23dcbbc80a91800a289aa9f8fd3cd347aba33245
Tom Coleman <tom@thesnail.org>
2016-03-02 15:04:20 +1100

    Version bump, update beta version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'check-npm-versions',
-  version: '0.0.1',
+  version: '0.1.1',
   summary: 'Check that required npm packages are installed at the app level',
   git: 'https://github.com/tmeasday/check-npm-versions.git',
   documentation: 'README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Npm.depends({'semver': '5.1.0'});
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3-modules-beta.7');
+  api.versionsFrom('1.3-beta.11');
   api.use('ecmascript');
   api.mainModule('check-npm-versions.js');
 });
