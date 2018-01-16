13dd0caeeac0d4fab9e968f295d042ec419fdedf
Tom Coleman <tom@thesnail.org>
2018-01-17 10:10:13 +1100

    Release 0.3.2

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'tmeasday:check-npm-versions',
-  version: '0.3.1',
+  version: '0.3.2',
   summary: 'Check that required npm packages are installed at the app level',
   git: 'https://github.com/tmeasday/check-npm-versions.git',
-  documentation: 'README.md'
+  documentation: 'README.md',
 });
 
-Npm.depends({'semver': '5.1.0'});
+Npm.depends({ semver: '5.1.0' });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.3-beta.11');
