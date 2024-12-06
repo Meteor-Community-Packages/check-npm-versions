54525841c0bfac9deb01ab69648a5ae2504e57a9
9Morello <caiomorello@protonmail.ch>
2024-12-06 14:58:23 -0500

    Update package.js
    Upgrade `semver` to the latest version

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 /* global Package:readonly, Npm:readonly  */
 Package.describe({
   name: 'tmeasday:check-npm-versions',
-  version: '2.0.0',
+  version: '2.0.1',
   summary: 'Check that required npm packages are installed at the app level',
   git: 'https://github.com/tmeasday/check-npm-versions.git',
   documentation: 'README.md',
 });
 
-Npm.depends({ semver: '6.3.0' }); // 7.x versions are incompatible with Internet Explorer
+Npm.depends({ semver: '7.6.3' });
 
 Package.onUse(function (api) {
   api.versionsFrom(['2.8.0', '3.0.1']);
